2a673cbba4c306697c1d9f4cf6fd0a1d10c8c7eb
Ayush Negi <Indianmusicghoul@gmail.com>
2025-05-28 17:42:53 +0530

    added comments to java file.

--- src/main/java/com/ayush/Project1/services/Productservice.java
+++ src/main/java/com/ayush/Project1/services/Productservice.java
@@ -6,6 +6,7 @@ import java.util.List;
 import com.ayush.Project1.exceptions.ProductNotFoundException;
 import com.ayush.Project1.models.Product;
 
+// this is the interface class and it is implemented by fakestoreProductService and SelfProductService.
 public interface Productservice {
 
     Product getSingleProduct(long productID) throws ProductNotFoundException;
